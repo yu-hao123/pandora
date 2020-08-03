1c2721976e1cb810296c7d7f912d3a3e069006ad
yu-hao123 <yuhaowangxia@gmail.com>
2020-08-02 21:40:45 -0300

    feat: sets up occlusion modeling dataset

---
+++ src/create_dataset.m
@@ -0,0 +1,47 @@
+% Create the objects for modeling the occlusion maneuver
+%% Dividing the dataset into fit and validation
+
+close all;
+clear variables;
+
+load('../data/signals/corrected_occl_1921.mat');
+
+fit_ratio = 0.7; % fit to validation ratio
+samples = size(c_paw, 1);
+fit_idx = round(samples*fit_ratio);
+
+fit_paw = c_paw(1:fit_idx);
+fit_pes = c_pes(1:fit_idx);
+
+val_paw = c_paw(fit_idx:end);
+val_pes = c_pes(fit_idx:end);
+
+total_paw = c_paw;
+total_pes = c_pes;
+
+%% Data objects
+
+fit_data = iddata(fit_paw, fit_pes, Ts * 10^-3);
+fit_data.InputName = 'Esophageal Pressure';
+fit_data.OutputName = 'Airway Pressure';
+fit_data.InputUnit = 'cmH20';
+fit_data.OutputUnit = 'cmH20';
+fit_data.Tstart = 0.0;
+
+val_data = iddata(val_paw, val_pes, Ts * 10^-3);
+val_data.InputName = 'Esophageal Pressure';
+val_data.OutputName = 'Airway Pressure';
+val_data.InputUnit = 'cmH20';
+val_data.OutputUnit = 'cmH20';
+val_data.Tstart = 0.0;
+
+total_data = iddata(total_paw, total_pes, Ts * 10^-3);
+total_data.InputName = 'Esophageal Pressure';
+total_data.OutputName = 'Airway Pressure';
+total_data.InputUnit = 'cmH20';
+total_data.OutputUnit = 'cmH20';
+total_data.Tstart = 0.0;
+
+save("../data/models/occl_1921_dataset.mat",...
+    'total_data','val_data', 'fit_data');
+
