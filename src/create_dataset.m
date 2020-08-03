8f51e8beba63c420e83ba469e379f05c51b8d000
yu-hao123 <yuhaowangxia@gmail.com>
2020-08-02 23:01:16 -0300

    feat: applies a lowpass 4Hz filter to the occlusion dataset

--- src/create_dataset.m
+++ src/create_dataset.m
@@ -45,3 +45,50 @@ total_data.Tstart = 0.0;
 save("../data/models/occl_1921_dataset.mat",...
     'total_data','val_data', 'fit_data');
 
+figure;
+plot(total_data);
+
+%% Filtered Dataset
+
+smooth_paw = lowpass(c_paw, 4, 1 / (Ts * 10^-3));
+smooth_pes = lowpass(c_pes, 4, 1 / (Ts * 10^-3));
+one_percent = round(samples/100);
+smooth_paw = smooth_paw(one_percent:samples-one_percent);
+smooth_pes = smooth_pes(one_percent:samples-one_percent);
+fit_idx = round(size(smooth_paw, 1) * fit_ratio);
+
+smooth_fit_paw = smooth_paw(1:fit_idx);
+smooth_fit_pes = smooth_pes(1:fit_idx);
+
+smooth_val_paw = smooth_paw(fit_idx:end);
+smooth_val_pes = smooth_pes(fit_idx:end);
+
+smooth_total_paw = smooth_paw;
+smooth_total_pes = smooth_pes;
+
+fit_data = iddata(smooth_fit_paw, smooth_fit_pes, Ts * 10^-3);
+fit_data.InputName = 'Esophageal Pressure';
+fit_data.OutputName = 'Airway Pressure';
+fit_data.InputUnit = 'cmH20';
+fit_data.OutputUnit = 'cmH20';
+fit_data.Tstart = 0.0;
+
+val_data = iddata(smooth_val_paw, smooth_val_pes, Ts * 10^-3);
+val_data.InputName = 'Esophageal Pressure';
+val_data.OutputName = 'Airway Pressure';
+val_data.InputUnit = 'cmH20';
+val_data.OutputUnit = 'cmH20';
+val_data.Tstart = 0.0;
+
+total_data = iddata(smooth_total_paw, smooth_total_pes, Ts * 10^-3);
+total_data.InputName = 'Esophageal Pressure';
+total_data.OutputName = 'Airway Pressure';
+total_data.InputUnit = 'cmH20';
+total_data.OutputUnit = 'cmH20';
+total_data.Tstart = 0.0;
+
+save("../data/models/smooth_occl_1921_dataset.mat",...
+    'total_data','val_data', 'fit_data');
+
+figure;
+plot(total_data);
