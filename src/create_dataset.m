d60a903a18d8a3ded980e7b46c0ed53feee9bc8a
yu-hao123 <yuhaowangxia@gmail.com>
2020-08-05 23:58:31 -0300

    feat: updates lowpass cutoff frequency to 0.3Hz adds offset and delay correction to the pes and paw waveforms re-estimate the corresponding models

--- src/create_dataset.m
+++ src/create_dataset.m
@@ -6,6 +6,18 @@ clear variables;
 
 load('../data/signals/corrected_occl_1921.mat');
 
+delay = 20; % delay (samples)
+gain = max(c_paw) - min(c_paw);
+
+% remove delay
+c_paw = c_paw(1:end-delay);
+c_pes = c_pes(delay+1:end);
+
+%remove offset
+c_paw = c_paw - max(c_paw);
+c_pes = c_pes - max(c_pes);
+
+
 fit_ratio = 0.7; % fit to validation ratio
 samples = size(c_paw, 1);
 fit_idx = round(samples*fit_ratio);
@@ -13,8 +25,8 @@ fit_idx = round(samples*fit_ratio);
 fit_paw = c_paw(1:fit_idx);
 fit_pes = c_pes(1:fit_idx);
 
-val_paw = c_paw(fit_idx:end);
-val_pes = c_pes(fit_idx:end);
+val_paw = c_paw(fit_idx+1:end);
+val_pes = c_pes(fit_idx+1:end);
 
 total_paw = c_paw;
 total_pes = c_pes;
@@ -35,7 +47,7 @@ val_data.InputName = 'Esophageal Pressure';
 val_data.OutputName = 'Airway Pressure';
 val_data.InputUnit = 'cmH20';
 val_data.OutputUnit = 'cmH20';
-val_data.Tstart = size(fit_data,1)* Ts * (10^-3);
+val_data.Tstart = (size(fit_data,1))* Ts * (10^-3);
 
 total_data = iddata(total_paw, total_pes, Ts * 10^-3);
 total_data.InputName = 'Esophageal Pressure';
@@ -54,8 +66,8 @@ plot(total_data);
 
 % Passband filter 25 rad/s?
 
-smooth_paw = lowpass(c_paw, 2, fs);
-smooth_pes = lowpass(c_pes, 2, fs);
+smooth_paw = lowpass(c_paw, 0.3, fs);
+smooth_pes = lowpass(c_pes, 0.3, fs);
 one_percent = round(samples/200);
 smooth_paw = smooth_paw(one_percent:samples-one_percent);
 smooth_pes = smooth_pes(one_percent:samples-one_percent);
@@ -64,8 +76,8 @@ fit_idx = round(size(smooth_paw, 1) * fit_ratio);
 smooth_fit_paw = smooth_paw(1:fit_idx);
 smooth_fit_pes = smooth_pes(1:fit_idx);
 
-smooth_val_paw = smooth_paw(fit_idx:end);
-smooth_val_pes = smooth_pes(fit_idx:end);
+smooth_val_paw = smooth_paw(fit_idx+1:end);
+smooth_val_pes = smooth_pes(fit_idx+1:end);
 
 smooth_total_paw = smooth_paw;
 smooth_total_pes = smooth_pes;
@@ -82,7 +94,7 @@ val_data.InputName = 'Esophageal Pressure';
 val_data.OutputName = 'Airway Pressure';
 val_data.InputUnit = 'cmH20';
 val_data.OutputUnit = 'cmH20';
-val_data.Tstart = size(fit_data,1)*Ts*(10^-3);
+val_data.Tstart = (size(fit_data,1))*Ts*(10^-3);
 
 total_data = iddata(smooth_total_paw, smooth_total_pes, Ts * 10^-3);
 total_data.InputName = 'Esophageal Pressure';
